4a16f917f9a19edb9a08973c5711dec175eda594
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2021-04-20 10:46:19 +0100

    chore: add logout endpoint

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware(['auth:sanctum']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::resource('/users', UserController::class);
