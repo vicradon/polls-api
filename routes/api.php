faec65c03f53a9c1ca4e4dc4b29c66e8748870ae
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2021-04-21 07:17:36 +0100

    refactor: remove tutorial materials

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware(['auth:sanctum']);
-Route::get('/some-new-route', function () {
-    return "I'm new";
-});
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::resource('/users', UserController::class);
